5cd70d53ea3b1c397dc2b5f1fddab18330f24aa2
trevorius <trevorius45@hotmail.com>
2020-11-19 06:17:13 -0500

    created last endpoint put completed

--- Controllers/InterventionsController.cs
+++ Controllers/InterventionsController.cs
@@ -44,11 +44,11 @@ namespace Intervention_management.Controllers
             return intervention;
         }
 
-        // PUT: api/Interventions/5
+        // PUT: api/changet-status-to-in-progress/{id}
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("inprogress/{id}")]
-        public async Task<IActionResult> PutInProgressStatusIntervention(long id, Intervention intervention)
+        [HttpPut("change-status-to-in-progress/{id}")]
+        public async Task<ActionResult<Intervention>> PutInProgressStatusIntervention(long id, Intervention intervention)
         {
             //save intervention in a temporary variable
             var tmpIntervention = _context.interventions.Where(e => e.Id == intervention.Id).FirstOrDefault<Intervention>();
@@ -92,9 +92,60 @@ namespace Intervention_management.Controllers
                 }
             }
 
-            return NoContent();
+            return intervention;
         }
 
+        //PUT api/change-status-to-completed/{id}
+        [HttpPut("change-status-to-completed/{id}")]
+        public async Task<ActionResult<Intervention>> PutCompletedIntervention(long id, Intervention intervention)
+        {
+            //save intervention in a temporary variable
+            var tmpIntervention = _context.interventions.Where(e => e.Id == intervention.Id).FirstOrDefault<Intervention>();
+
+            if (id != intervention.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(tmpIntervention).State = EntityState.Detached;
+            _context.Entry(intervention).State = EntityState.Modified;
+
+            try
+            {
+                intervention.Id = tmpIntervention.Id;
+                intervention.start_date = tmpIntervention.start_date;
+                intervention.end_date = DateTime.Now;
+                intervention.result = tmpIntervention.result;
+                intervention.report = tmpIntervention.report;
+                intervention.status = "Completed";
+                intervention.created_at = tmpIntervention.created_at;
+                intervention.updated_at = DateTime.Now;
+                intervention.author = tmpIntervention.author;
+                intervention.customer_id = tmpIntervention.customer_id;
+                intervention.building_id = tmpIntervention.building_id;
+                intervention.employee_id = tmpIntervention.employee_id;
+                intervention.battery_id = tmpIntervention.battery_id;
+                intervention.column_id = tmpIntervention.column_id;
+                intervention.elevator_id = tmpIntervention.elevator_id;
+
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InterventionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return intervention;
+        }
+
+        
+        
         // get: api/interventions/pending-interventions
         [HttpGet("pending-interventions")]
         public ActionResult<List<Intervention>> GetPendingInterventions()
@@ -115,33 +166,7 @@ namespace Intervention_management.Controllers
             return PendingInterventions;
         }
 
-        // POST: api/Interventions
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<Intervention>> PostIntervention(Intervention intervention)
-        {
-            _context.interventions.Add(intervention);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetIntervention", new { id = intervention.Id }, intervention);
-        }
-
-        // DELETE: api/Interventions/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Intervention>> DeleteIntervention(long id)
-        {
-            var intervention = await _context.interventions.FindAsync(id);
-            if (intervention == null)
-            {
-                return NotFound();
-            }
-
-            _context.interventions.Remove(intervention);
-            await _context.SaveChangesAsync();
-
-            return intervention;
-        }
+        
 
         private bool InterventionExists(long id)
         {
